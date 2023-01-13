8facd81bf38d6850868c44f8845d3e6caf7d7d3a
yasyaindra <indrayasya96@gmail.com>
2023-01-13 13:53:03 +0800

    dataset

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import tensorflow as tf
-import streamlit as st
 
 
 @st.cache(allow_output_mutation=True)
 def load_model():
   model=tf.keras.models.load_model('my_model2.hdf5')
   return model
+
 with st.spinner('Model is being loaded..'):
   model=load_model()
 
